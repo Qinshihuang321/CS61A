920ba6cc856d6277f613b75083c4cb5f7003db42
unknown <l3022724604@gmail.com>
2023-07-31 00:06:27 +0800

    hw03

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ f = lambda start , goal : goal[0] + start
 times_per_player = [[1, 3, 5], [2, 5, 6]]
 a = [3]
 b = []
-b += [1]
+b += [3]
 b += [2]
 
-print(a[0]+b[0])
+for i in range(len(b)):
+    print(i)
